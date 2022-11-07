59d4281389388ad92676b682dfb23c861f07006b
missvader <vanepalazon611@gmail.com>
2022-11-07 13:02:54 +0100

    Exercise 1. Create first component

---
+++ src/components/escena/Escena.js
@@ -0,0 +1,7 @@
+import React from "react";
+function Escena(){
+  return(
+    <p>El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial</p>
+    )
+  };
+export default Escena;
